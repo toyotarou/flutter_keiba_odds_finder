1027ab8779c0b5c3b991707ea6ad4448e9727d96
toyodahideyuki <toyodahideyuki4819@gmail.com>
2026-05-31 11:27:33 +0900

    commit 20260531 1127

--- lib/controllers/controllers_mixin.dart
+++ lib/controllers/controllers_mixin.dart
@@ -3,7 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app_param/app_param.dart';
 import 'horse/horse.dart';
 import 'laravel_config/laravel_config.dart';
+import 'netkeiba_odds/netkeiba_odds.dart';
+import 'netkeiba_race/netkeiba_race.dart';
 import 'odds/odds.dart';
+import 'odds_get_timing/odds_get_timing.dart';
 import 'race/race.dart';
 import 'schedule/schedule.dart';
 
@@ -49,6 +52,30 @@ mixin ControllersMixin<T extends ConsumerStatefulWidget> on ConsumerState<T> {
   LaravelConfigState get laravelConfigState => ref.watch(laravelConfigProvider);
 
   LaravelConfig get laravelConfigNotifier => ref.read(laravelConfigProvider.notifier);
+
+  //==========================================//
+
+  //==========================================//
+
+  NetkeibaOddsState get netkeibaOddsState => ref.watch(netkeibaOddsProvider);
+
+  NetkeibaOdds get netkeibaOddsNotifier => ref.read(netkeibaOddsProvider.notifier);
+
+  //==========================================//
+
+  //==========================================//
+
+  NetkeibaRaceState get netkeibaRaceState => ref.watch(netkeibaRaceProvider);
+
+  NetkeibaRace get netkeibaRaceNotifier => ref.read(netkeibaRaceProvider.notifier);
+
+  //==========================================//
+
+  //==========================================//
+
+  OddsGetTimingState get oddsGetTimingState => ref.watch(oddsGetTimingProvider);
+
+  OddsGetTiming get oddsGetTimingNotifier => ref.read(oddsGetTimingProvider.notifier);
 
   //==========================================//
 }
